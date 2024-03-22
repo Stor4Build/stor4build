4152424e140376a6fc3851c97143f17e88ea22ab
Jason W. DeGraw <jason.degraw@gmail.com>
2024-03-22 14:09:24 -0400

    Work on the API

--- src/s4b/api/__init__.py
+++ src/s4b/api/__init__.py
@@ -34,7 +34,7 @@ def create_app(config=None, instance_path=None):
 
     #
     @app.route('/prototype', methods=['GET', 'POST'])
-    def protootype_route():
+    def prototype_route():
         # POST request
         if request.method == 'POST':
             prototype = request.form.get('prototype')
@@ -84,6 +84,78 @@ def create_app(config=None, instance_path=None):
 </form>''' % (''.join(['<option value="%s">%s</option>' % (el,el) for el in stor4build.prototypes_list]),
               ''.join(['<option value="%s">%s</option>' % (el,el) for el in stor4build.climate_zone_list]),
               ''.join(['<option value="%s">%s</option>' % (el,el) for el in stor4build.vintage_values]))
+
+    #
+    @app.route('/tank', methods=['GET', 'POST'])
+    def tank_route():
+        # POST request
+        if request.method == 'POST':
+            prototype = request.form.get('prototype')
+            cz = 'ASHRAE 169-2006-%s' % request.form.get('cz')
+            vintage = request.form.get('vintage')
+            charge_start = request.form.get('charge_start')
+            charge_end = request.form.get('charge_end')
+            discharge_start = request.form.get('discharge_start')
+            discharge_end = request.form.get('discharge_end')
+            charge_temp = request.form.get('charge_temp')
+            ntanks = request.form.get('ntanks')
+            trim_temp = request.form.get('trim_temp')
+            epw = stor4build.weather_lookup(cz)
+            osw = {
+                'measure_paths': [ os.path.abspath(app.config['MEASURES_DIR']) ],
+                'run_directory': os.path.join(app.instance_path, 'run'),
+                'seed_file': stor4build.prototype_lookup(prototype, cz, vintage),
+                'steps': [
+                    {
+                        "measure_dir_name" : "add_pytank",
+                        "name" : "Add Python Tank",
+                        "description" : "This measure will add the Python tank model.",
+                        "modeler_description" : "This measure will add the Python tank model.",
+                        "arguments" : {
+                            "chrg_start" : charge_start,
+                            "chrg_end" : charge_end,
+                            "dchrg_start" : discharge_start,
+                            "dchrg_end" : discharge_end,
+                            "chrg_temp" : charge_temp,
+                            "num_tanks" : ntanks,
+                            "trim_temp" : trim_temp
+                        }
+                    }
+                ],
+                'weather_file': epw
+            }
+            stor4build.run(app.config['OPENSTUDIO'], app.instance_path, osw)
+
+            return '''
+<h1>%s</h1>
+<h2>Climate zone: %s</h2>
+<h2>Vintage: %s</h2>
+''' % (prototype, cz, vintage)
+
+        # GET request
+        return '''
+<form method="POST">
+    <div><label>Prototype: <select id="prototype" name="prototype">
+        %s
+        </select></label></div>
+    <div><label>Climate Zone: <select id="cz" name="cz">
+        %s
+        </select></label></div>
+    <div><label>Vintage: <select id="vintage" name="vintage">
+        %s
+        </select></label></div>
+    <div><label>Charge Start Time: <input type="time" id="charge_start" name="charge_start" min="00:00" max="24:00" value="21:00" required /></label></div>
+    <div><label>Charge End Time: <input type="time" id="charge_end" name="charge_end" min="00:00" max="24:00" value="07:00" required /></label></div>
+    <div><label>Discharge Start Time: <input type="time" id="discharge_start" name="discharge_start" min="00:00" max="24:00" value="12:00" required /></label></div>
+    <div><label>Discharge End Time: <input type="time" id="discharge_end" name="discharge_end" min="00:00" max="24:00" value="18:00" required /></label></div>
+    <div><label>Charge Temperature: <input type="number" id="charge_temp" name="charge_temp" min="-10.0" max="10.0" value="-3.8" required /></label></div>
+    <div><label>Number of Tanks: <input type="number" id="ntanks" name="ntanks" min="1" step="1" max="50" value="1" required /></label></div>
+    <div><label>Trim Temperature: <input type="number" id="trim_temp" name="trim_temp" min="0.0" max="20.0" value="10.0" required /></label></div>
+    <input type="submit" value="Submit">
+</form>''' % (''.join(['<option value="LargeOffice">Large Office</option>']),
+              ''.join(['<option value="%s">%s</option>' % (el,el) for el in stor4build.climate_zone_list]),
+              ''.join(['<option value="%s">%s</option>' % (el,el) for el in stor4build.vintage_values]))
+
 
     return app
 
--- src/s4b/util.py
+++ src/s4b/util.py
@@ -14,3 +14,8 @@ def weather_lookup(climate_zone):
     path = os.path.dirname(os.path.realpath(__file__))
     # Cheat for now, all the world is 4A
     return os.path.join(path, '..', '..', 'resources', 'USA_TN_Knoxville-McGhee.Tyson.AP.723260_TMY3.epw')
+
+def prototype_lookup(type, climate_zone, vintage):
+    path = os.path.dirname(os.path.realpath(__file__))
+    # Cheat for now, all buildings are this one large office
+    return os.path.join(path, '..', '..', 'resources', 'LargeOffice.osm')
