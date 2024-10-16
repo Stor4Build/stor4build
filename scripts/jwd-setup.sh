0c310c8fbca047059fe1012ea90a24549b783892
Jason W. DeGraw <jason.degraw@gmail.com>
2024-10-16 02:42:38 -0400

    Minor fix

--- scripts/jwd-setup.sh
+++ scripts/jwd-setup.sh
@@ -1,4 +1,4 @@
 export FLASK_TIMESCALE_HOST="localhost"
-export FLASK_TIMESCALE_DB="timeseries"
+export FLASK_TIMESCALE_DB="tessbed"
 export FLASK_TIMESCALE_USERNAME="postgres"
 export FLASK_TIMESCALE_PASSWORD="postgres"
