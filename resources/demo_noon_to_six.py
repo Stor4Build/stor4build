43ffb83c43390c88bac9d0633ac9c3f30f751dd9
Jason W. DeGraw <jason.degraw@gmail.com>
2025-11-07 08:17:46 -0500

    Initial implementation of the basic demo

---
+++ resources/demo_noon_to_six.py
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2024-present Oak Ridge National Laboratory, managed by UT-Battelle, Alliance for Sustainable Energy, LLC, and contributors
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from pyenergyplus.plugin import EnergyPlusPlugin
+from case_details import MODE_SCHEDULE_TYPE, MODE_SCHEDULE_NAME, Mode
+
+class NoonToSix(EnergyPlusPlugin):
+
+    def actuate(self, state, x):
+        self.api.exchange.set_actuator_value(state, self.data['mode_schedule'], x)
+
+    def on_begin_timestep_before_predictor(self, state) -> int:
+        if 'mode_schedule' not in self.data:
+            self.data['mode_schedule'] = self.api.exchange.get_actuator_handle(
+                state, MODE_SCHEDULE_TYPE, "Schedule Value", MODE_SCHEDULE_NAME
+            )
+            if self.data['mode_schedule'] == -1:
+                self.api.runtime.issue_severe(state, "Could not get handle to TES mode schedule")
+                return 1
+
+        hour = self.api.exchange.hour(state)
+        if hour < 12:
+            self.actuate(state, Mode.CHARGE)
+        elif 12 <= hour < 18:
+            self.actuate(state, Mode.DISCHARGE)
+        else:
+            self.actuate(state, Mode.CHARGE)
+        return 0
