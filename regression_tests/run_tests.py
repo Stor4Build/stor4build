433f125fd51355786c2a19954a1e31e10fd721a3
Jason W. DeGraw <jason.degraw@gmail.com>
2026-01-26 06:39:38 -0500

    Add large hotel

--- regression_tests/run_tests.py
+++ regression_tests/run_tests.py
@@ -24,7 +24,7 @@ with open(cases_file, 'r') as fp:
 
 for name,case in inputs.items():
     print(name)
-    if 'Warehouse' not in name:
+    if 'LargeHotel' not in name:
         continue
     for type, info in case.items():
         if type == 'api':
