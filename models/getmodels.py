20531c4429cdd8316bcfac504520d910a9066081
Jason W. DeGraw <jason.degraw@gmail.com>
2026-08-11 10:20:59 -0400

    Update the model retrieval script

--- models/getmodels.py
+++ models/getmodels.py
@@ -16,12 +16,27 @@ resultsdb = stor4build.ResultsDatabase(database = "stor4build",
                                        weather_table = 'weather',
                                        verbose = True)
                                        
-prototypes = {'LargeOffice': {'cz': ['3A', '4A', '5A'], 'vintage': ['2013', '2019', 'pre1980']},
-              'SmallOffice': {'cz': ['3A', '4A', '5A'], 'vintage': ['2010', 'post1980', '2019']},
-              'RetailStandalone': {'cz': ['3A', '4A', '5A'], 'vintage': ['2007', '2004', '2016']}}
+prototypes = {'College': (('3A', '2007'),),
+              'Courthouse': (('5B', 'post1980'),),
+              'FullServiceRestaurant': (('3A', '2010'), ('3A', 'pre1980')),
+              'Hospital': (('5A', 'post1980'),),
+              'Laboratory': (('5A', '2004'),),
+              'LargeDataCenterHighITE': (('3A', '2019'),),
+              'LargeDataCenterLowITE': (('5A', '2004'),),
+              'LargeHotel': (('5A', '2016'),),
+              'LargeOffice': (('3A', '2013'), ('4A', '2019'), ('5A', 'pre1980')),
+              'MediumOffice': (('5A', '2004'),),
+              'Outpatient': (('3A', '2013'),),
+              'PrimarySchool': (('5B', '2016'),),
+              'QuickServiceRestaurant': (('3A', '2019'),),
+              'RetailStandalone': (('3A', '2007'), ('4A', '2004'), ('5A', '2016')),
+              'RetailStripmall': (('5B', '2007'),),
+              'SecondarySchool': (('3A', '2010'),),
+              'SmallOffice': (('4A', 'post1980'), ('5A', '2019')),
+              'Warehouse': (('3A', '2010'),)}
               
-for type, value in prototypes.items():
-    for cz,vintage in zip(value['cz'], value['vintage']):
+for type, pairs in prototypes.items():
+    for cz,vintage in pairs:
         filename = type + '_' + cz + '_' + vintage + '.osm'
         print(filename)
         result = resultsdb.get_model(filename, building_type=type, climate_zone=cz, vintage=vintage)
